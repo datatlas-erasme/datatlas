6428f03d1700f6223081e0fd37c02fc1d7b29cd5
lutangar <johan.dufour@gmail.com>
2023-01-11 20:53:35 +0100

    chore(front): show off usage of a few Kepler components

--- apps/frontend/src/pages/ProjectsPage.tsx
+++ apps/frontend/src/pages/ProjectsPage.tsx
@@ -1,3 +1,38 @@
 import React from 'react';
+import Checkbox from 'kepler.gl/dist/components/common/checkbox';
+import styled, { ThemeProvider } from 'styled-components';
+import { Tooltip, Button, TextArea } from 'kepler.gl/dist/components/common/styled-components';
+import { Add } from 'kepler.gl/dist/components/common/icons';
+import { theme as basicTheme } from 'kepler.gl/dist/styles/base';
 
-export const ProjectsPage = () => <div>Projects</div>;
+const Spaced = styled.article`
+  padding: 15px;
+`;
+
+export const ProjectsPage = () => {
+  return (
+    <div>
+      <ThemeProvider theme={basicTheme}>
+        <h1>Projects</h1>
+        <Spaced>
+          <Checkbox id="test" label="label" checked disabled={false} />
+        </Spaced>
+        <Spaced>
+          <Tooltip id={'id'} place="left" effect="solid">
+            <span>Message</span>
+          </Tooltip>
+        </Spaced>
+        <Spaced>
+          <Button>
+            <Add height="12px" />
+            Button
+          </Button>
+        </Spaced>
+        <Spaced>
+          Textarea
+          <TextArea />
+        </Spaced>
+      </ThemeProvider>
+    </div>
+  );
+};
