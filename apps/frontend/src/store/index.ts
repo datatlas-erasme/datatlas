a0bfc0ef2024cd07303810c82208f91c5ca50d0a
lutangar <johan.dufour@gmail.com>
2023-01-17 13:49:54 +0100

    feat(frontend): setup app state persistence to local storage

--- apps/frontend/src/store/index.ts
+++ apps/frontend/src/store/index.ts
@@ -1,13 +1,21 @@
 import { createLogger } from 'redux-logger';
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import { enhanceReduxMiddleware } from 'kepler.gl';
-import { reducer } from './reducers';
+import { reducer as rootReducer } from './reducers';
 import { initialState } from './reducers';
 
 const actionsBlacklist = ['@@kepler.gl/MOUSE_MOVE', '@@kepler.gl/UPDATE_MAP', '@@kepler.gl/LAYER_HOVER'];
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer,
+  reducer: persistedReducer,
   preloadedState: initialState,
   middleware: (getDefaultMiddleware) => [
     ...getDefaultMiddleware({
@@ -23,6 +31,6 @@ const store = configureStore({
   devTools: process.env.NODE_ENV !== 'production',
 });
 
-export type RootState = ReturnType<typeof store.getState>;
+const persistor = persistStore(store);
 
-export default store;
+export { store, persistor };
