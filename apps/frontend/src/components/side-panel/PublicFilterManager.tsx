dbb7319498df7b5bc24d46caa17dd5573638ea53
lutangar <johan.dufour@gmail.com>
2023-01-18 11:42:35 +0100

    chore(frontend): WIP PUBLIC FILTER KEPLER

--- apps/frontend/src/components/side-panel/PublicFilterManager.tsx
+++ apps/frontend/src/components/side-panel/PublicFilterManager.tsx
@@ -1,16 +1,69 @@
-import React from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { injectIntl, FormattedDate, WrappedComponentProps } from 'react-intl';
+import { Datasets, Layer } from 'kepler.gl/src';
+import { Action } from '@reduxjs/toolkit';
 
-interface Props extends WrappedComponentProps {
+interface PublicFilterManagerProps extends WrappedComponentProps {
   date: Date | number;
+  filters: object[];
+  datasets?: Datasets;
+  layers: Layer[];
+  actions: {};
 }
 
-const PublicFilterManager = (props: Props) => {
-  const { date, intl } = props;
+const PublicFilterManager = ({ actions, date, intl, datasets, filters, layers }: PublicFilterManagerProps) => {
+  const { addFilter, enlargeFilter, removeFilter, setFilter, toggleAnimation, toggleFilterFeature, showDatasetTable } =
+    actions;
+  const isAnyFilterAnimating = filters.some((f) => f.isAnimating);
+  const hadEmptyFilter = filters.some((f) => !f.name);
+  const hadDataset = Object.keys(datasets).length;
+  const onClickAddFilter = useCallback(() => {
+    const defaultDataset = (Object.keys(datasets).length && Object.keys(datasets)[0]) || null;
+    addFilter(defaultDataset);
+  }, [datasets, addFilter]);
+  // render last added filter first
+  const reversedIndex = useMemo(() => {
+    return new Array(filters.length)
+      .fill(0)
+      .map((d, i) => i)
+      .reverse();
+  }, [filters.length]);
+
   return (
-    <span title={intl.formatDate(date)}>
-      <FormattedDate value={date} />
-    </span>
+    <div className="filter-manager">
+      <span title={intl.formatDate(date)}>
+        <FormattedDate value={date} />
+      </span>
+      <SourceDataCatalog datasets={datasets} showDatasetTable={showDatasetTable} />
+      <SidePanelDivider />
+      <SidePanelSection>
+        {reversedIndex.map((idx) => (
+          <FilterPanel
+            key={`${filters[idx].id}-${idx}`}
+            idx={idx}
+            filters={filters}
+            filter={filters[idx]}
+            datasets={datasets}
+            layers={layers}
+            isAnyFilterAnimating={isAnyFilterAnimating}
+            removeFilter={() => removeFilter(idx)}
+            enlargeFilter={() => enlargeFilter(idx)}
+            toggleAnimation={() => toggleAnimation(idx)}
+            toggleFilterFeature={() => toggleFilterFeature(idx)}
+            setFilter={setFilter}
+          />
+        ))}
+      </SidePanelSection>
+      <Button
+        className="add-filter-button"
+        inactive={hadEmptyFilter || !hadDataset}
+        width="105px"
+        onClick={onClickAddFilter}
+      >
+        <Add height="12px" />
+        <FormattedMessage id={'filterManager.addFilter'} />
+      </Button>
+    </div>
   );
 };
 
