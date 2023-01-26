fbe9a5e479b509c8d7f40dc58a8681c080e0b9a1
Yassin Siouda <yassin@siouda.com>
2023-01-26 16:53:11 +0100

    chore(frontend): Get Post Put Del projects

--- apps/backend/src/project/project.controller.ts
+++ apps/backend/src/project/project.controller.ts
@@ -1,23 +1,34 @@
-import { Controller, Get, Post, Put, Delete, Res, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Res, Body, Param } from '@nestjs/common';
 import { Repository } from 'typeorm';
 //import { AppService } from '../app/app.service';
 import { ProjectService } from './project.service';
 import { Project} from './project.entity';
 
 
-@Controller('projects')
+@Controller()
 export class ProjectController {
     constructor(private readonly projectService: ProjectService) { }
 
-    @Get()
+    @Get('projects')
     async fetchAll(): Promise<Project[]> {
         return await this.projectService.findAll();
     }
 
-    @Post()
+    @Post('project')
     async create(@Body() project: Project): Promise<Project> {
         return await this.projectService.create(project);
     }
+
+    @Delete('project/:id')
+    async delete(@Param('id') id: number): Promise<void> {
+        return await this.projectService.delete(id);
+    }
+
+    @Put('project/:id')
+    async update(@Param('id') id: number, @Body() project: Project): Promise<Project> {
+        return await this.projectService.update(project);
+    }
+    
 
 
 }
