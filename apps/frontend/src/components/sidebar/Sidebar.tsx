b2c4ec5577a4fb39970da680e30b101e8bb5724d
Olivier PIERRE <olivier@opierre.fr>
2023-01-31 12:06:45 +0100

    feat(front): markup

--- apps/frontend/src/components/sidebar/Sidebar.tsx
+++ apps/frontend/src/components/sidebar/Sidebar.tsx
@@ -1,5 +1,9 @@
 import React, { ReactNode } from 'react';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import { StartNewProjectForm } from '../forms/StartNewProjectForm';
+import { startNewProject } from '../../store/reducers/app/drafts';
+import ProjectCard from '../card/ProjectCard';
 
 interface SideBarInterface {
   children: ReactNode;
@@ -10,11 +14,27 @@ const SideBarContainer = styled.aside`
   padding: ${(props) => props.theme.layoutsBoxContainer};
 `;
 
-const Sidebar = ({ children }: SideBarInterface) => (
-  <SideBarContainer>
-    <h3>Je suis la SideBar</h3>
-    {children}
-  </SideBarContainer>
-);
+const Sidebar = () => {
+  const dispatch = useDispatch();
+  return (
+    <SideBarContainer>
+      <h2>Démarrer un nouveau projet :</h2>
+      <form>
+        <div>
+          <label>1. Entrez le nom du projet</label>
+          <input />
+        </div>
+        <div>
+          <label>2. Sélectionnez un fond de carte</label>
+          <StartNewProjectForm onSubmit={(data) => dispatch(startNewProject(data))} />
+        </div>
+        <div>
+          <label>3. Choisissez un modèle</label>
+          <ProjectCard id={'idCard1'} name={'default'} draft={true} datasets={[]} ownerId={1} />
+        </div>
+      </form>
+    </SideBarContainer>
+  );
+};
 
 export default Sidebar;
