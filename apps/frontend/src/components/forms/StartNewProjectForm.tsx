052b60af0355204115065c42da452290a1c82ce1
Olivier PIERRE <olivier@opierre.fr>
2023-02-01 14:45:57 +0100

    chore(front): add style and intl

--- apps/frontend/src/components/forms/StartNewProjectForm.tsx
+++ apps/frontend/src/components/forms/StartNewProjectForm.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { FormattedMessage } from 'react-intl';
+import { startNewProject } from '../../store/reducers/app/drafts';
+import ProjectCard from '../card/ProjectCard';
+import { Button } from '../buttons';
 
 export interface StartNewProjectFormProps {
   onSubmit: SubmitHandler<StartNewProjectFormData>;
@@ -21,15 +24,29 @@ export function StartNewProjectForm({ onSubmit }: StartNewProjectFormProps) {
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor={'nameProject'}>
+        1.
+        <FormattedMessage defaultMessage={'Entrez le nom du projet'} />
+      </label>
       {/* register your input into the hook by invoking the "register" function */}
       <input defaultValue="" {...register('name', { required: true })} />
       {errors.name && <FormattedMessage defaultMessage="This field is required" />}
-
+      <label htmlFor={'selectMap'}>
+        2.
+        <FormattedMessage defaultMessage={'Sélectionnez un fond de carte'} />
+      </label>
       <select {...register('mapStyle')}>
         <option value="satellite">satellite</option>
         <option value="topo">topo</option>
       </select>
 
+      <div>
+        <label htmlFor={'selectModel'}>
+          3.
+          <FormattedMessage defaultMessage={'Choisissez un modèle'} />
+        </label>
+        <ProjectCard id={'idCard1'} name={'default'} draft={true} datasets={[]} ownerId={1} />
+      </div>
       <input type="submit" />
     </form>
   );
--- apps/frontend/src/components/sidebar/Sidebar.tsx
+++ apps/frontend/src/components/sidebar/Sidebar.tsx
@@ -1,38 +1,32 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { StartNewProjectForm } from '../forms/StartNewProjectForm';
 import { startNewProject } from '../../store/reducers/app/drafts';
 import ProjectCard from '../card/ProjectCard';
-
-interface SideBarInterface {
-  children: ReactNode;
-}
+import { Button } from '../buttons';
+import { Simulate } from 'react-dom/test-utils';
+import submit = Simulate.submit;
 
 const SideBarContainer = styled.aside`
   grid-area: aside;
   padding: ${(props) => props.theme.layoutsBoxContainer};
+  form {
+    display: flex;
+    flex-direction: column;
+  }
 `;
 
 const Sidebar = () => {
+  const intl = useIntl();
   const dispatch = useDispatch();
   return (
     <SideBarContainer>
-      <h2>Démarrer un nouveau projet :</h2>
-      <form>
-        <div>
-          <label>1. Entrez le nom du projet</label>
-          <input />
-        </div>
-        <div>
-          <label>2. Sélectionnez un fond de carte</label>
-          <StartNewProjectForm onSubmit={(data) => dispatch(startNewProject(data))} />
-        </div>
-        <div>
-          <label>3. Choisissez un modèle</label>
-          <ProjectCard id={'idCard1'} name={'default'} draft={true} datasets={[]} ownerId={1} />
-        </div>
-      </form>
+      <h2>
+        <FormattedMessage defaultMessage={'Démarrer un nouveau projet :'} />
+      </h2>
+      <StartNewProjectForm onSubmit={(data) => dispatch(startNewProject(data))} />
     </SideBarContainer>
   );
 };
