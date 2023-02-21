8826919dd58bfcc838af3403cb85bb8f96b097ce
lutangar <johan.dufour@gmail.com>
2023-02-21 15:46:20 +0100

    chore: add selectProjectById and show of usage
    > for advanced usages see :
    > https://redux.js.org/usage/deriving-data-selectors#calling-selectors-with-parameters

--- apps/frontend/src/pages/ProjectPage.tsx
+++ apps/frontend/src/pages/ProjectPage.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import KeplerMap from '../components/KeplerMap';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import KeplerMap from '../components/KeplerMap';
+import { selectProjectById } from '../store/selectors';
+import { RootState } from '../store/reducers';
 
 export const ProjectPage = () => {
   const { id } = useParams();
+  const project = useSelector<RootState>((state) => selectProjectById(state, id));
+  console.log('project', project);
 
   if (!id) {
     throw new Error('404');
