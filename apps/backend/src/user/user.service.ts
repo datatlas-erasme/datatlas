c24bc616ee4dbd6194d98ce84bb41e8c9c1c3a42
RACINE Pierre-Alexandre <pierrealexandreracine@gmail.com>
2023-02-28 09:56:18 +0100

    Update user.service.ts

--- apps/backend/src/user/user.service.ts
+++ apps/backend/src/user/user.service.ts
@@ -12,10 +12,7 @@ export class UserService {
   ) {}
 
   /**
-   * Adds a user into the database.
    * TODO : passwords are stored clearly in database -> hash them !
-   * @param userDto Private user data.
-   * @return number New user_id or 0 if user already exists.
    */
   async createUser(userDto: UserDto): Promise<number> {
     const user = new UserEntity(userDto.username, userDto.password, userDto.role, userDto.active);
@@ -26,11 +23,6 @@ export class UserService {
         });
   }
 
-  /**
-   * Get public user data from its user_id
-   * @param id
-   * @return UserPublicDTO public user data {id, username, role, is_active}
-   */
   async getUser(id = 0): Promise<UserPublicDTO> {
     return this.userRepository
       .findOne({ id })
@@ -54,33 +46,18 @@ export class UserService {
     });
   }
 
-  /**
-   * Deletes user. Returns void.
-   * @param userId
-   * @return void
-   */
   async deleteUserById(userId: number): Promise<void> {
     return this.userRepository.nativeDelete(userId).then(() => {
       return;
     });
   }
 
-  /**
-   * Is the username already in database ?
-   * @param username
-   * @return boolean
-   */
   async isUsernameAlreadyInDatabase(username: string): Promise<boolean> {
     return this.userRepository.findOne({ username }).then((user) => {
       return user != null;
     });
   }
 
-  /**
-   * Returns the user id for given username. Returns 0 if the username isn't found or incoherent.
-   * @param username The user_name
-   * @return number The user_id
-   */
   async getUserIDByUserName(username: string): Promise<number> {
     return await this.userRepository.findOne({ username }).then((user) => {
       if (user === null) {
