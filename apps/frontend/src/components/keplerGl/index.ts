e123953ea485ee57e86a4bac3a55de4231492dbb
lutangar <johan.dufour@gmail.com>
2023-03-10 16:15:39 +0100

    feat(sidebar): begin Kepler side bar customization
    - Hide panel header
    - Add a custom action to the list of the actions available in the layer list
    - Hide datasets list
    - Change "Add layer" button to "Add dataset"

--- apps/frontend/src/components/keplerGl/index.ts
+++ apps/frontend/src/components/keplerGl/index.ts
@@ -1,4 +1,11 @@
 import { injectComponents } from 'kepler.gl';
+import { replaceLayerManager } from './factories/LayerManagerFactory';
 import { replaceLoadDataModal } from './factories/LoadDataModalFactory';
+import { replaceLayerPanelHeader } from './factories/LayerPanelHeaderFactory';
 
-export const KeplerGl = injectComponents([replaceLoadDataModal()]);
+export const KeplerGl = injectComponents([
+  replaceLoadDataModal(),
+  replaceLayerManager(),
+  replaceLayerPanelHeader(),
+  replaceLayerManager(),
+]);
