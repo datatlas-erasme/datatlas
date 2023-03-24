33b1a0546ee4fd3eb432c05bcaa222e7a9410756
Olivier PIERRE <olivier@opierre.fr>
2023-03-24 16:07:51 +0100

    feat(front): footer modification and legal mention page

--- apps/frontend/src/pages/LoginPage.tsx
+++ apps/frontend/src/pages/LoginPage.tsx
@@ -1,12 +1,12 @@
 import React, { useRef, useState } from 'react';
 import styled from 'styled-components';
+import { FormattedMessage } from 'react-intl';
 import { Layout } from './layouts';
 import { LoginForm } from '../components/forms';
 import { DatatlasLogoSquare } from '../components/logos';
 import FirstShapeBg from '../assets/shapes/first-shape.svg';
 import SecondShapeBg from '../assets/shapes/second-shape.svg';
 import { StyledLabel } from '../components/forms';
-import { FormattedMessage } from 'react-intl';
 import GithubLink from '../components/buttons/GithubLink';
 import SidePanelButton from '../components/buttons/SidePanelButton';
 
