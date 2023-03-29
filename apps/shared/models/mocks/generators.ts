a0bc6e275a85c94480cf83aafcd05cc86771c187
Johan Dufour <johan.dufour@gmail.com>
2023-03-29 14:19:10 +0200

    chore(mocks): add fake datasets generators (#119)

--- apps/shared/models/mocks/generators.ts
+++ apps/shared/models/mocks/generators.ts
@@ -1,5 +1,7 @@
 import { faker } from '@faker-js/faker';
-import { KeplerMapConfig, ProjectInterface, UserInterface } from '../';
+import { DatasetInterface, KeplerMapConfig, ProjectInterface, UserInterface } from '../';
+
+export const generateArray = (l = faker.datatype.number(25)) => Array.from(Array(faker.datatype.number(l)).keys());
 
 export const generateFakeUser = (partialUser: Partial<UserInterface> = {}): UserInterface => ({
   id: faker.datatype.number(100),
@@ -9,11 +11,45 @@ export const generateFakeUser = (partialUser: Partial<UserInterface> = {}): User
   ...partialUser,
 });
 
+export const generateFakeDataset = (partialDataset: Partial<DatasetInterface> = {}): DatasetInterface => {
+  const id = faker.datatype.string(5);
+  return {
+    id,
+    url: faker.internet.url(),
+    version: 'v1',
+    checksum: faker.datatype.string(),
+    updatedAt: faker.date.past(),
+    warning: faker.lorem.sentence(5),
+    data: generateFakeDatasetData({ id }),
+    ...partialDataset,
+  };
+};
+
+export const generateFakeDatasetData = (
+  partialDatasetData: Partial<DatasetInterface['data']> = {}
+): DatasetInterface['data'] => ({
+  id: faker.datatype.string(5),
+  allData: generateArray().map(() => [[generateFakeGeoJsonFeature(), faker.animal.bear()]]),
+  label: 'Bears',
+  color: faker.color.rgb({ format: 'decimal' }),
+  fields: [
+    { name: '_geojson', type: 'geojson' },
+    { name: 'bear', type: 'string' },
+  ],
+  ...partialDatasetData,
+});
+
+export const generateFakeGeoJsonFeature = () => ({
+  geometry: { type: 'Point', coordinates: [faker.address.latitude(), faker.address.longitude()] },
+  type: 'Feature',
+  properties: {},
+});
+
 export const generateFakeProject = (partialProject: Partial<ProjectInterface> = {}): ProjectInterface => ({
   id: faker.datatype.number(100),
   title: faker.animal.insect(),
   description: faker.lorem.lines(1),
-  datasets: [],
+  datasets: generateArray(2).map(() => generateFakeDataset()),
   owner: generateFakeUser(),
   createdAt: faker.date.past(),
   draft: faker.datatype.boolean(),
@@ -22,6 +58,4 @@ export const generateFakeProject = (partialProject: Partial<ProjectInterface> = 
   config: new KeplerMapConfig(),
   ...partialProject,
 });
-
-export const generateFakeProjects = () =>
-  Array.from(Array(faker.datatype.number(50)).keys()).map((id) => generateFakeProject({ id }));
+export const generateFakeProjects = () => generateArray().map((id) => generateFakeProject({ id }));
