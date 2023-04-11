cc88ae87ccd7d9f4f294425acba6aad37d31b51f
lutangar <johan.dufour@gmail.com>
2023-04-11 18:38:24 +0200

    chore(api): test documenting swagger post parameters

--- apps/backend/src/app/app.controller.ts
+++ apps/backend/src/app/app.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
+import { Controller, Post, UseGuards, Body } from '@nestjs/common';
 import { LocalAuthGuard } from '../auth/local-auth.guard';
 import { AuthService } from '../auth/auth.service';
+import { ApiOperation, ApiProperty, ApiResponse } from '@nestjs/swagger';
+
+export class LoginDto {
+  @ApiProperty()
+  username: string;
+
+  @ApiProperty()
+  password: string;
+}
 
 @Controller()
 export class AppController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
+  @ApiOperation({ summary: 'Create cat' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
   @Post('auth/login')
-  async login(@Request() req) {
-    return await this.authService.login(req.body);
+  async login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
   }
 }
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -13,15 +13,6 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder()
-    .setTitle('Datatlas API')
-    .setDescription('The datatlas API description')
-    .setVersion('0.1')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-
   await app.get(MikroORM).getSchemaGenerator().ensureDatabase();
   await app.get(MikroORM).getSchemaGenerator().updateSchema();
 
@@ -30,6 +21,17 @@ async function bootstrap() {
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Datatlas API')
+    .setDescription('The datatlas API description')
+    .setVersion('0.1')
+    .setBasePath('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
