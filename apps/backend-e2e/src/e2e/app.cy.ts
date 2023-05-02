02e4eebe27cadd835fb2751812c3a9c4c974ee22
paracine <pierrealexandreracine@gmail.com>
2023-05-02 10:01:16 +0200

    feat: test if can reach API doc

--- apps/backend-e2e/src/e2e/app.cy.ts
+++ apps/backend-e2e/src/e2e/app.cy.ts
@@ -1,6 +1,5 @@
 describe('Backend', () => {
   beforeEach(() => cy.visit('/api/'));
-
   it('Should display the API documentation', () => {
     cy.get('h2').should('contain.text', 'Datatlas API');
   });
