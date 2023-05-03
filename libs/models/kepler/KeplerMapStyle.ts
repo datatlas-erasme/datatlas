674faf347f205e1dd590db154c9fde7f12e7879c
lutangar <johan.dufour@gmail.com>
2023-05-03 20:08:47 +0200

    chore(backend): project creation validation
    chore(backend): project creation validation
    
    chore(backend): project creation validation

--- libs/models/kepler/KeplerMapStyle.ts
+++ libs/models/kepler/KeplerMapStyle.ts
@@ -2,62 +2,63 @@ import { BaseMapStyle } from 'kepler.gl/reducers';
 import { SavedMapStyle } from 'kepler.gl/schemas/schema-manager';
 import { hexToRgb } from 'kepler.gl/dist/utils';
 import { DEFAULT_LAYER_GROUPS, ICON_PREFIX } from 'kepler.gl/dist/constants/default-settings';
-
-export const DEFAULT_MAP_STYLES: BaseMapStyle[] = [
-  {
-    id: 'dark',
-    label: 'Dark',
-    url: 'mapbox://styles/uberdata/cjoqbbf6l9k302sl96tyvka09',
-    icon: `${ICON_PREFIX}/UBER_DARK_V2.png`,
-    layerGroups: DEFAULT_LAYER_GROUPS,
-  },
-  {
-    id: 'light',
-    label: 'Light',
-    url: 'mapbox://styles/uberdata/cjoqb9j339k1f2sl9t5ic5bn4',
-    icon: `${ICON_PREFIX}/UBER_LIGHT_V2.png`,
-    layerGroups: DEFAULT_LAYER_GROUPS,
-  },
-  {
-    id: 'muted',
-    label: 'Muted Light',
-    url: 'mapbox://styles/uberdata/cjfyl03kp1tul2smf5v2tbdd4',
-    icon: `${ICON_PREFIX}/UBER_MUTED_LIGHT.png`,
-    layerGroups: DEFAULT_LAYER_GROUPS,
-  },
-  {
-    id: 'muted_night',
-    label: 'Muted Night',
-    url: 'mapbox://styles/uberdata/cjfxhlikmaj1b2soyzevnywgs',
-    icon: `${ICON_PREFIX}/UBER_MUTED_NIGHT.png`,
-    layerGroups: DEFAULT_LAYER_GROUPS,
-  },
-  {
-    id: 'satellite',
-    label: 'Satellite',
-    url: `mapbox://styles/mapbox/satellite-v9`,
-    icon: `${ICON_PREFIX}/UBER_SATELLITE.png`,
-    layerGroups: [],
-  },
-];
+import { SavedCustomMapStyle } from 'kepler.gl/src/schemas/schema-manager';
 
 export class KeplerMapStyle implements SavedMapStyle {
+  public static readonly DEFAULT_MAP_STYLES: BaseMapStyle[] = [
+    {
+      id: 'dark',
+      label: 'Dark',
+      url: 'mapbox://styles/uberdata/cjoqbbf6l9k302sl96tyvka09',
+      icon: `${ICON_PREFIX}/UBER_DARK_V2.png`,
+      layerGroups: DEFAULT_LAYER_GROUPS,
+    },
+    {
+      id: 'light',
+      label: 'Light',
+      url: 'mapbox://styles/uberdata/cjoqb9j339k1f2sl9t5ic5bn4',
+      icon: `${ICON_PREFIX}/UBER_LIGHT_V2.png`,
+      layerGroups: DEFAULT_LAYER_GROUPS,
+    },
+    {
+      id: 'muted',
+      label: 'Muted Light',
+      url: 'mapbox://styles/uberdata/cjfyl03kp1tul2smf5v2tbdd4',
+      icon: `${ICON_PREFIX}/UBER_MUTED_LIGHT.png`,
+      layerGroups: DEFAULT_LAYER_GROUPS,
+    },
+    {
+      id: 'muted_night',
+      label: 'Muted Night',
+      url: 'mapbox://styles/uberdata/cjfxhlikmaj1b2soyzevnywgs',
+      icon: `${ICON_PREFIX}/UBER_MUTED_NIGHT.png`,
+      layerGroups: DEFAULT_LAYER_GROUPS,
+    },
+    {
+      id: 'satellite',
+      label: 'Satellite',
+      url: `mapbox://styles/mapbox/satellite-v9`,
+      icon: `${ICON_PREFIX}/UBER_SATELLITE.png`,
+      layerGroups: [],
+    },
+  ];
+
   mapStyles = {};
   styleType = 'dark';
   topLayerGroups = {};
   threeDBuildingColor = hexToRgb('#D1CEC7');
   visibleLayerGroups = {};
 
-  constructor(styleType: BaseMapStyle['id'] = 'dark') {
+  constructor(styleType: BaseMapStyle['id'] = 'dark', mapStyles?: SavedCustomMapStyle) {
     this.styleType = styleType;
-    this.mapStyles = KeplerMapStyle.createMapStyles();
+    this.mapStyles = mapStyles || KeplerMapStyle.createMapStyles();
   }
 
   static createMapStyles() {
-    return DEFAULT_MAP_STYLES.reduce(
-      (accu: Record<string, BaseMapStyle>, curr: BaseMapStyle) => ({
+    return KeplerMapStyle.DEFAULT_MAP_STYLES.reduce(
+      (accu: SavedCustomMapStyle, curr: BaseMapStyle) => ({
         ...accu,
-        [curr.id]: { ...curr, accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN },
+        [curr.id]: { ...curr, accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN as string, custom: false },
       }),
       {}
     );
