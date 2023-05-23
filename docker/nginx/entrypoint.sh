fe9cfbbbe507bb0118b29e357ee64f0c2c58c0b2
yassin@siouda.com <yassin@siouda.com>
2023-05-23 14:58:55 +0200

    Revert "chore(docker): use sed to inject env vars"
    This reverts commit 89f019835500fc1518dbc96f7af0c66d8d5ff2f3.

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,9 +1,9 @@
-# replace in static/js replace defaultMapboxToken in js folder with the actual token
-sed -i "s/defaultMapboxToken/$REACT_APP_MAPBOX_ACCESS_TOKEN/g" /usr/share/nginx/html/static/js/*.js
-# same with defaultLocale
-sed -i "s/defaultLocale/$REACT_APP_DEFAULT_LOCALE/g" /usr/share/nginx/html/static/js/*.js
-# same with defaultApiBaseUrl
-sed -i "s/defaultApiBaseUrl/$REACT_APP_API_BASE_URL/g" /usr/share/nginx/html/static/js/*.js
+# create an .env-config.js file in the static/js folder
+echo "window._env_ = {" > /usr/share/nginx/html/static/js/.env-config.js 
+echo "  REACT_APP_MAPBOX_ACCESS_TOKEN: \"$REACT_APP_MAPBOX_ACCESS_TOKEN\"," >> /usr/share/nginx/html/static/js/.env-config.js
+echo "  REACT_APP_DEFAULT_LOCALE: \"$REACT_APP_DEFAULT_LOCALE\"," >> /usr/share/nginx/html/static/js/.env-config.js
+echo "  REACT_APP_API_BASE_URL: \"$REACT_APP_API_BASE_URL\"" >> /usr/share/nginx/html/static/js/.env-config.js
+echo "};" >> /usr/share/nginx/html/static/js/.env-config.js
 
 
 # add the env-config.js file to the index.html file
