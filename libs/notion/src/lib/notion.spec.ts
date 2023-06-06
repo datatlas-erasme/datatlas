082258755f1a8ec83be4150719d676785abe25e0
paracine <pierrealexandreracine@gmail.com>
2023-06-06 16:54:03 +0200

    feat: extracting data from datasets
    #100

--- libs/notion/src/lib/notion.spec.ts
+++ libs/notion/src/lib/notion.spec.ts
@@ -1,7 +1,15 @@
 import { notion } from './notion';
+import * as fs from 'fs';
 
-describe('notion', () => {
-  it('should work', () => {
-    expect(notion()).toEqual('notion');
+describe('Test : extraction of data from a notion page.', () => {
+  it('Should get content of the notion page.', () => {
+    try {
+      const jsonString = fs.readFileSync('./libs/notion/src/lib/data_test.json', 'utf-8'); // todo make sure the path is built dynamically ie its base is the parent directory of the present file
+      const jsonData = JSON.parse(jsonString);
+      //expect(typeof notion(jsonData)).toBe({"fields":Array<string>,"rows":Array<object>,});
+      expect(typeof notion(jsonData)).toBe('object');
+    } catch (err) {
+      console.error(err);
+    }
   });
 });
--- libs/notion/src/lib/notion.ts
+++ libs/notion/src/lib/notion.ts
@@ -1,4 +1,53 @@
 // takes notionJson return GeoJson
-export function notion(): string {
-  return 'notion';
+export function notion(jsonData: { object: string; results: Array<{ properties: object }> }): {
+  fields: Array<{ name; format; type }>;
+  rows: string[];
+} {
+  const notionFieldnames: Array<string> = [];
+  const notionFields: Array<{ name; format; type }> = [];
+  const rows = [];
+
+  // FIRST, GRAB ALL THE FIELD NAMES.
+  for (const row of jsonData.results) {
+    const keys = Object.keys(row.properties);
+    for (const key in keys) {
+      if (!notionFieldnames.includes(keys[key])) {
+        notionFieldnames.push(keys[key]);
+      }
+    }
+  }
+  // SECONDLY, CREATE ALL THE FIELD DECLARATION.
+  for (const fieldName in notionFieldnames) {
+    notionFields.push({
+      name: notionFieldnames[fieldName],
+      format: '',
+      type: typeTranslationNotionToGeoJson(jsonData.results[0]['properties'][notionFieldnames[fieldName]]),
+    });
+  }
+
+  console.log(notionFields);
+  /*
+  // THEN GRAB THE DATA
+  for (const row of jsonData.results) {
+    for (const field in notionFields){
+      console.log(notionFields[field]);
+      console.log(row.properties[notionFields[field]]);
+    }
+  }*/ /*
+  return {
+    fields: notionFields,
+    rows,
+  };*/
+  return {
+    fields: notionFields,
+    rows: [],
+  };
+}
+
+export function typeTranslationNotionToGeoJson(notionData: object): string {
+  // Might be more complete later ?
+  if (notionData['type'] === 'number') {
+    return 'real';
+  }
+  return 'string';
 }
