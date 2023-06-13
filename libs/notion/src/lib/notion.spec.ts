16565111411abe02208ffbe82be295d7eda7f432
paracine <pierrealexandreracine@gmail.com>
2023-06-13 09:55:58 +0200

    feat: explanation how to delete warnings in jest

--- libs/notion/src/lib/notion.spec.ts
+++ libs/notion/src/lib/notion.spec.ts
@@ -1,6 +1,12 @@
 import { notion } from './notion';
 import * as fs from 'fs';
 
+// Launch it using : jest path/to/project/datatlas/libs/notion/src/lib/notion.spec.ts
+
+// Many warnings may be raised using jest>=29.0.0
+// Reverting back to any jest 28 version seems to solve this problem.
+// See more at this jest issue : https://github.com/jestjs/jest/issues/13576
+
 describe('Test : extraction of data from a notion page.', () => {
   it('Should get content of the notion page.', () => {
     try {
