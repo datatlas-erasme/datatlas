4e3454666f7fe98ea855232d4bfa50d2e096bc4d
paracine <pierrealexandreracine@gmail.com>
2023-07-25 08:21:12 +0200

    feat: try of better logger

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -8,12 +8,38 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { MikroORM } from '@mikro-orm/core';
 import * as bodyParser from 'body-parser';
-
+import { WinstonModule } from 'nest-winston';
+import { transports, format } from 'winston';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['verbose'],
+    logger: WinstonModule.createLogger({
+      transports: [
+        // let's log errors into its own file
+        new transports.File({
+          filename: `logs/error.log`,
+          level: 'error',
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+        // logging all level
+        new transports.File({
+          filename: `logs/combined.log`,
+          format: format.combine(format.timestamp(), format.json()),
+        }),
+        // we also want to see logs in our console
+        new transports.Console({
+          format: format.combine(
+            format.cli(),
+            format.splat(),
+            format.timestamp(),
+            format.printf((info) => {
+              return `${info.timestamp} ${info.level}: ${info.message}`;
+            })
+          ),
+        }),
+      ],
+    }),
   });
   app.enableCors({
     credentials: true,
